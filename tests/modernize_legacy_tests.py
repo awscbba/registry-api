9a0d7a44904d97af1a3f7ff0cef52cc5f94d2d50
Sergio Rodriguez <sergio@ffactory.io>
2025-08-05 14:35:07 -0400

    style: apply black formatting to modernize_legacy_tests.py

--- tests/modernize_legacy_tests.py
+++ tests/modernize_legacy_tests.py
@@ -17,7 +17,9 @@ import sys
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../src"))
 
-from src.services.defensive_dynamodb_service import DefensiveDynamoDBService as DynamoDBService
+from src.services.defensive_dynamodb_service import (
+    DefensiveDynamoDBService as DynamoDBService,
+)
 
 
 def get_actual_method_signatures():
